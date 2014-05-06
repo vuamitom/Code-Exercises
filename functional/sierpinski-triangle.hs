c8f9519dc1c01d51d239dfd7f7edaadb820c1f14
Tam <vumhtam@gmail.com>
2014-05-07 01:15:22 +0700

    update

--- functional/sierpinski-triangle.hs
+++ functional/sierpinski-triangle.hs
@@ -1,19 +1,30 @@
 type Row = [Char]
 type Matrix = [Row]
 
-fill::  Row -> Int -> Int -> Int -> Int-> Row 
-fill r s b d pad 
+
+fill::  Int -> Row -> Int -> Int -> Int -> Int-> Row 
+fill 63 r s b d pad 
     | s > (length r - pad) = r 
-    | otherwise = fill [ if inrange $ snd p then '1' else  fst p | p <- pairs ] (s + b + d ) b d pad 
-        where 
-            pairs = zip r [0..]
-            inrange x = x >= s && x < (s + b) 
+    | otherwise = fill 63 [ if inrange $ snd p then '1' else  fst p | p <- pairs ] (s + b + d ) b d pad 
+    where
+    pairs = zip r [0..]
+    inrange x = x >= s && x < (s + b) 
+   
+fill w r s b d pad 
+    | s > (length r - pad) = r 
+    | otherwise = fill w [ got p | p <- pairs ] (s + b + d ) b d pad 
+    where
+    got x = if snd x >= s && snd x <( s+b+d) then update x else fst x
+    update x = if (inrange $ snd x) && fst x == '1' then '1' else '_'
+    pairs = zip r [0..]
+    inrange x = x >= s && x < (s + b) 
+
 initM :: Matrix 
 initM = [r | _ <- [1..32]]
     where r = ['_' | _ <- [1..63]]
 
 fillMatrix :: Matrix -> Int ->  Matrix 
-fillMatrix m n = [fill (snd p) (start $ fst p ) ( base $ fst p) ( distance (fst p)) (start $ fst p) | p <- ( withIdx  m)]  
+fillMatrix m n = [fill n (snd p) (start $ fst p ) ( base $ fst p) ( distance (fst p)) (start $ fst p) | p <- ( withIdx  m)]  
 --fillMatrix m n = [show ( distance $ fst p) | p <- ( withIdx  m)]  
     where 
         start = (+) 0 
@@ -28,9 +39,12 @@ reverseM = foldl (\acc x -> x : acc ) []
 prLs [] = putStr "" 
 prLs (x:xs) = do {putStrLn x; prLs xs; }
 
+superFill m n t = if t <n then superFill h n (t+1) else h where h = fillMatrix m  (width t)
+solve n = superFill (initM) n 0
 main = do 
     n <- readLn :: IO Int
     --print $ width n
-    prLs $reverseM $ fillMatrix ( initM ) (width n)  
+    --prLs $reverseM $ fillMatrix ( initM ) (width n)  
+    prLs $reverseM $ solve n 
     --print $ fill ['_' | _<- [1..63]] 1 13 3 
     
