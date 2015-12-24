d9b6c25bd8a928c8828c509f24a0c5181f1fb2d1
Vu Minh Tam <tamvu89@gmail.com>
2015-12-24 12:52:25 +0900

    concurrency eg

---
+++ concurrency/src/main/java/indi/tamvm/blog/App.java
@@ -0,0 +1,13 @@
+package indi.tamvm.blog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
