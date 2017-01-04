f1976475fb445aab8105f9407da576543de2277b
Tam Vu <tamvu@Tams-MacBook-Pro-2.local>
2017-01-04 22:33:46 +0700

    haskell factorial tail recursion

--- codeforces/tools.hs
+++ codeforces/tools.hs
@@ -14,3 +14,7 @@ toB x = t x []
         t y r 
             | y `mod` 2 == 1 = t (y `div` 2) (1:r)
             | otherwise = t (y `div` 2) (0:r)
+fact n = f n 1
+    where 
+        f 0 r = r
+        f n r = f (n - 1) (r * n)
