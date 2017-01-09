d007ff0a4b544ab0511efce396a7c63cbea68c30
Tam Vu <tamvu@Tams-MacBook-Pro-2.local>
2017-01-09 23:29:58 +0700

    failed test3

--- hackercup17/fight_zombie.py
+++ hackercup17/fight_zombie.py
@@ -1,22 +1,26 @@
 import math
 import re
+def pad(s):
+    return s + ('0'*(6 - len(s) + s.find('.') + 1))
 def ps(x, y, c):
     m  = 1
     for i in range (x - c + 1, x + 1):
         m *= i
-    return m / ((y ** x) * 1.0)
+    return m*1.0 * ((y - 1)** (x -c)) / math.factorial(c) / (y ** x) 
 
 def cal(x, y, d):
-    print 'cal ' + str(x) + ' ' + str(y) + ' ' + str(d)
-    if d <= x:
-        print 'ret'
+    #print 'cal ' + str(x) + ' ' + str(y) + ' ' + str(d)
+    if d <= x or d == 0:
+        #print 'ret'
         return 1
     elif d > ( y * x ):
         return 0
     else:
         s = 0
         for i in range (1, x + 1):
-            s += ps ( x, y , i ) * cal ( x- i, y - 1, d - (y * i)) 
+            r = ps ( x, y , i ) * cal ( x- i, y - 1, d - (y * i)) 
+            #print 'r = ' + str(r) + ' for ' + str(i) + ' of ' + str(y)
+            s += r
         s += cal ( x, y - 1, d) * (( (y - 1) / (y * 1.0)) ** x )
         return s
 
@@ -34,6 +38,6 @@ def solve():
             x, y, z = int(x), int(y), int(z)
             ps = cal(x, y, d - z)
             m = ps if ps > m else m
-        print 'Case #' + str(i + 1) + ': ' + str(m)
+        print 'Case #' + str(i + 1) + ': ' + pad(str(round(m, 6)))
 
 solve()
