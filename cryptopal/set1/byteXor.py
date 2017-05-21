dcbd0369388acb1639b445ec82c797ade0b0ff56
tamvm <tamvu89@gmail.com>
2017-05-22 00:18:59 +0700

    challenge 4

--- cryptopal/set1/byteXor.py
+++ cryptopal/set1/byteXor.py
@@ -1,9 +1,7 @@
 
 
-a = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'.decode('hex')
-b = [ord(c) for c in a]
-r = ''
-s = 0
+# a = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'.decode('hex')
+
 
 def score(s):
 	# vowels are scored higher
@@ -18,12 +16,35 @@ def score(s):
 	return t
 
 
+def guess(a):
+	# if True: return a 
+	# if not len(a) == 60:
+	# 	raise Exception('smth wrong, str len = ' + str(len(a)))
 
-for c in range(0, 256):
-	o = [chr(i ^ c) for i in b]
-	m = score(o)
-	r = ''.join(o) if m > s else r 
-	s = m if m > s else s
-	# print ''.join(o) + ' score = ' + str(m)
-print r 
+	b = [ord(c) for c in a]
+	# b = [ord(c) for c in 'ETAOIN SHRDLU']
+	r = ''
+	s = 0
+	for c in range(0, 256):
+		o = [chr(i ^ c) for i in b]
+		m = score(o)
+		r = ''.join(o) if m > s else r 
+		s = m if m > s else s
+		# print ''.join(o) + ' score = ' + str(m) + ' char = ' + str(c)
+	#print r 
+	return (r, s)
+
+
+# print guess('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'.decode('hex'))
+# if True: 
+# 	exit(0)
+lines = None
+with open('4.txt', 'r') as f:
+	lines = f.readlines()
+# print lines
+for l in lines: 
+	# print l
+	v, s = guess(l.strip('\n').decode('hex'))
+	if s >= 40:
+		print v + ' score = ' + str(s)
 
