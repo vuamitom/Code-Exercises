a5273a22f4c23498a10620ce0c125e263c8ab7df
tamvm <tamvu89@gmail.com>
2017-06-04 08:57:28 +0700

    wip

--- cryptopal/set1/break_repeate_xor.py
+++ cryptopal/set1/break_repeate_xor.py
@@ -16,27 +16,47 @@ def hamming_distance(a, b):
 def guess_key_length(ip):
 	m = 99999
 	keysize = 0
-
+	al = []
 	for s in range(2, 41):
 		w1 = ip[0:s]
 		w2 = ip[s:2*s]
-		d = hamming_distance(w1, w2)/s
+		d = hamming_distance(w1, w2)/ (s * 1.0)
+		al.append((s, d))
 		if d < m:
 			m = d
 			keysize = s
+	al =  sorted(al, key= lambda x: x[1])
 	print 'min hamming_distance = ' + str(m)
-	return keysize
+	return al[15:20]
 
 def solve(inp):
-	keysize = guess_key_length(inp)
-	bl = [[]] * keysize
-	key = []
-	for i, c in enumerate(inp):
-		bl[i % keysize].append(c)
-	for b in bl:
-		v, s, k = byteXor.guess(b)
-		key.append(k)
-	return key
+	keysizes = guess_key_length(inp)
+	for ks in keysizes:#range(1, 5):
+		keysize = ks[0]
+		# print ' key_size = ' + str(keysize)
+		bl = [[] for i in range(0, keysize)]
+		# print ' initial = ' + str(bl) + ' inp len = ' + str(len(inp))
+		key = []
+		# test = {0: 0, 1:0, 2:0, 3: 0, 4:0}
+		for i, c in enumerate(inp):
+			# print 'b no = ' + str(i % keysize)
+			# test[i % keysize] += 1
+			bl[i % keysize].append(c)
+		# print 'test = ' + str(test)
+		# print ' all size = ' + str([len(b) for b in bl])
+		log = True
+		for b in bl:
+			# print 'block len = ' + str(len(b))
+			v, s, k = byteXor.guess(b, log)
+			log = False
+			# print 'score = ' + str(s) + ' key = ' + str(k) + ' ' + v
+			# print ' val  = ' + v
+
+			key.append(k)
+		print 'key = ' + str(key)
+		r = decryptXOR(inp, key)
+		print r 
+		# break
 
 def decryptXOR(inp, key):
 	o = []
@@ -46,11 +66,13 @@ def decryptXOR(inp, key):
 
 
 # print hamming_distance('t', 'w')
-assert hamming_distance('this is a test', 'wokka wokka!!!') == 37
+x = hamming_distance('this is a test', 'wokka wokka!!!')
+assert  x == 37
+print 'normalize dist = ' + str(x * 1.0 / len('this is a test'))
 inp = None
 with open('6.txt', 'r') as f:
 	inp = f.read()
 
 inp = base64.b64decode(inp)
 key = solve(inp)
-print decryptXOR(inp, key)
+# print decryptXOR(inp, key)
--- cryptopal/set1/byteXor.py
+++ cryptopal/set1/byteXor.py
@@ -1,22 +1,50 @@
 
 
 # a = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'.decode('hex')
+freq = {
+	'a': 8.167, 'b': 1.492, 'c': 2.782, 'd': 4.253, 'e': 12.702, 'f': 2.228, 'g': 2.015,
+	'h': 6.094, 'i': 6.966, 'j': 0.153, 'k': 0.772, 'l': 4.025, 'm': 2.406,
+	'n': 6.749, 'o': 7.507, 'p': 1.929, 'q': 0.095, 'r': 5.987, 's': 6.327, 
+	't': 9.056, 'u': 2.758, 'v': 0.978, 'w': 2.360, 'x': 0.150, 'y': 1.974, 'z': 0.074
+}
 
+board = {}
+alp = sorted([x for x in range(ord('a'), ord('z') + 1)], key = lambda x: freq[chr(x)])
+for i, c in enumerate(alp): 
+	board[chr(c)] = i + 1
+# print ' score board = ' + str(board)
 
-def score(s):
+def score(s, log = False):
 	# vowels are scored higher
 	t = 0
+	histogram = {}
 	for i, c in enumerate(s):
-		if c.lower() in ['a', 'e', 'i', 'o', 'u']:
-			t += 3
-		elif ord(c.lower()) > ord('a') and ord(c.lower()) < ord('z'):
-			t += 1
+		a = c.lower()
+		# if ord(a) >= ord('a') and ord(a) <= ord('z'):
+		# 	if a not in histogram:
+		# 		histogram[a] = 1
+		# 	else:
+		# 		histogram[a] += 1
+		if a in board:
+			t += board[a]
+
+		# if c.lower() in ['a', 'e', 'i', 'o', 'u']:
+		# 	t += 3
+		# elif ord(c.lower()) > ord('a') and ord(c.lower()) < ord('z'):
+		# 	t += 1
 		elif c == ' ' and i > 0 and i < len(s) - 1 and s[i - 1].isalpha() and s[i+1].isalpha():
 			t += 1
+	# for k in histogram:
+	# 	f = histogram[k] * 100.0 / len(s)
+	# 	histogram[k] = f
+	# 	e = freq[k]
+	# 	t += 1 - abs(f - e) / e
+	# if log:
+	# 	print histogram
 	return t
 
 
-def guess(a):
+def guess(a, log = False):
 	# if True: return a 
 	# if not len(a) == 60:
 	# 	raise Exception('smth wrong, str len = ' + str(len(a)))
@@ -28,13 +56,13 @@ def guess(a):
 	k = None
 	for c in range(0, 256):
 		o = [chr(i ^ c) for i in b]
-		m = score(o)
+		m = score(o, c == 53)
 		r = ''.join(o) if m > s else r 
 		k = c if m > s else k
 		s = m if m > s else s
-		
-		# print ''.join(o) + ' score = ' + str(m) + ' char = ' + str(c)
-	#print r 
+		# if log and c < 31:
+			# print ''.join(o) + ' score = ' + str(m) + ' char = ' + str(c)
+	 
 	return (r, s, k)
 
 
@@ -46,9 +74,12 @@ if __name__ == '__main__':
 	with open('4.txt', 'r') as f:
 		lines = f.readlines()
 	# print lines
-	for l in lines: 
-		# print l
-		v, s, k = guess(l.strip('\n').decode('hex'))
-		if s >= 41:
-			print v + ' score = ' + str(s)
+	v, s, k = guess(lines[170].strip('\n').decode('hex'))
+	print v + ' score = ' + str(s) + ' key = ' + str(k) + ' line = '	
+
+	# for ix, l in enumerate(lines): 
+	# 	# print l
+	# 	v, s, k = guess(l.strip('\n').decode('hex'))
+		# if s >= 41:
+		# 	print v + ' score = ' + str(s) + ' key = ' + str(k) + ' line = ' + str(ix)
 
