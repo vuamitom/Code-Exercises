fad38a6b4497af4d364e725174cf70bbe48b48a8
tamvm <tamvu89@gmail.com>
2017-06-09 23:40:14 +0700

    aes ecb does not need iv vector

---
+++ cryptopal/set1/aes.py
@@ -0,0 +1,10 @@
+from Crypto.Cipher import AES
+import base64
+
+obj = AES.new("YELLOW SUBMARINE", AES.MODE_ECB, "0" * 16)
+content = None
+with open('7.txt', 'r') as f:
+	content = f.read()
+
+
+print obj.decrypt(base64.b64decode(content))
