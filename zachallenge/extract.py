54ac951fe17c0aff6e28733ba7f5485faf7c3719
tamvm <tamvu89@gmail.com>
2018-08-21 18:35:16 +0700

    update file name

--- zachallenge/extract.py
+++ zachallenge/extract.py
@@ -11,11 +11,33 @@ TRAIN_TEMP = os.path.join(BASE, 'temp')
 
 MIN_VOICE_FREQ = 70
 MAX_VOICE_FREQ = 280
-NO_FRAME = 500
+NO_FRAME = 10
 N_MFCC = 13
 N_MELS = 26
 FEATURE_SIZE = N_MFCC * 2 + N_MELS
-SAMPLE_PER_FRAME = 512
+SAMPLE_PER_FRAME = 256
+
+def get_mean():
+    file_list = os.listdir(TRAIN_INPUT)
+    prev_mean = None
+    for l in file_list:
+        p = os.path.join(TRAIN_INPUT, l) 
+        print ('process ', l)
+        for r in os.listdir(p):
+            fp = os.path.join(p, r)
+            # start = datetime.datetime.now()
+            features = extract_voice_feature(fp)
+            print ('feature size = ', features.shape)
+            # print ('extract takes ', (datetime.datetime.now() - start).total_seconds())
+            features = fix_size(features)
+            row, col = features.shape        
+            assert row == FEATURE_SIZE
+            assert col == NO_FRAME
+            # print ('output feature = ', features.shape)
+            local_mean = np.mean(features)
+            break
+            # print (features)
+        break
 
 def extract():
     try:        
@@ -28,29 +50,40 @@ def extract():
         print ('Already extracted')
         return None
 
-    file_list = os.listdir(TRAIN_INPUT)
-    dataset = np.ndarray(shape=(len(file_list), FEATURE_SIZE, NO_FRAME),
-                         dtype=np.float32)
-    m = dict()
-    for l in file_list:
-        p = os.path.join(TRAIN_INPUT, l) 
-        
-        for r in os.listdir(p):
+    for l in os.listdir(TRAIN_INPUT):
+        try:
+            os.mkdir(os.path.join(TRAIN_TEMP, l), mode=0o755)
+        except FileExistsError:
+            pass            
+        p = os.path.join(TRAIN_INPUT, l)
+        voice_list = os.listdir(p)
+        # dataset = np.ndarray(shape=(FEATURE_SIZE, NO_FRAME),
+        #                  dtype=np.float64)        
+        for idx, r in enumerate(voice_list):
             fp = os.path.join(p, r)
             # start = datetime.datetime.now()
             features = extract_voice_feature(fp)
             # print ('extract takes ', (datetime.datetime.now() - start).total_seconds())
             features = fix_size(features)
             # print (features)
-            c = len(features[0])
-            m[c] = 0 if c not in m else m[c]
-            m[c] += 1
-        print (m)
-            # break
-        # break 
-    print (m)
+            row, col = features.shape        
+            assert row == FEATURE_SIZE
+            assert col == NO_FRAME
+            # dataset[idx, :, :] = features
+            temp_filename = os.path.join(TRAIN_TEMP, l, str(idx) + '.pickle')            
+            try:
+                with open(temp_filename, 'wb') as f:
+                  pickle.dump(features, f, pickle.HIGHEST_PROTOCOL)
+                print ('done extract for file ', r)
+              except Exception as e:
+                print('Unable to save data to', temp_filename, ':', e)                
 
 def fix_size(features):
+    r, c = features.shape
+    if c < NO_FRAME:
+        return np.concatenate((features, features[:, :(NO_FRAME-c)]), axis=1)
+    elif c > NO_FRAME:
+        return features[:, :NO_FRAME]
     return features
 
 def extract_voice_feature(fp):
@@ -66,13 +99,13 @@ def extract_voice_feature(fp):
                         fmax=MAX_VOICE_FREQ, 
                         power=1, # energy instead of power
                         n_mels=N_MELS)
-    # mfccs = librosa.feature.mfcc(y=X, sr=sample_rate, 
-    #                     hop_length=SAMPLE_PER_FRAME, 
-    #                     fmin=MIN_VOICE_FREQ, 
-    #                     fmax=MAX_VOICE_FREQ,
-    #                     n_mels=N_MELS,
-    #                     n_mfcc=N_MFCC)
-    # delta = librosa.feature.delta(mfccs)
+    mfccs = librosa.feature.mfcc(y=X, sr=sample_rate, 
+                        hop_length=SAMPLE_PER_FRAME, 
+                        fmin=MIN_VOICE_FREQ, 
+                        fmax=MAX_VOICE_FREQ,
+                        n_mels=N_MELS,
+                        n_mfcc=N_MFCC)
+    delta = librosa.feature.delta(mfccs)
 
     # print (mfccs.shape)
     # print (spectrogram.shape)
@@ -84,11 +117,8 @@ def extract_voice_feature(fp):
     # plt.tight_layout()
     # mfccs = 
     # plt.show()
-    return spectrogram
-    
-def encode_label(l):
-    return (1, 1, 2)
+    return np.vstack((spectrogram, mfccs, delta))
 
 
 if __name__ == '__main__':
-    extract()
+    get_mean()
