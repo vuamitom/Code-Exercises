e4b6a0442615dc7b1d15df8532bd176007850e46
tamvm <tamvm@vng.com.vn>
2018-10-25 12:38:20 +0700

    add some test

---
+++ java_experiments/src/main/java/com/vng/zing/experiment/regex/OpenJDKRegex.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018-2022 by Zalo Group.
+ * All Rights Reserved.
+ */
+package com.vng.zing.experiment.regex;
+
+public class OpenJDKRegex {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java_experiments/src/test/java/com/vng/zing/experiment/tests/regex/PatternTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2018-2022 by Zalo Group.
+ * All Rights Reserved.
+ */
+package com.vng.zing.experiment.tests.regex;
+
+import com.vng.zing.experiment.regex.Matcher;
+import com.vng.zing.experiment.regex.Pattern;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class PatternTest {
+
+    @Test
+    public void testCompile() {
+        Pattern p = Pattern.compile("abc+d");
+        Matcher m = p.matcher("abccccd");
+        System.out.println(m.matches());
+        System.out.println(m.group(0));
+
+        Pattern p1 = Pattern.compile("abc+d|ab+cd");
+        Matcher m1 = p1.matcher("abccccd");
+        System.out.println(m1.matches());
+
+    }
+
+    @Test
+    public void compareSpeed() {
+        Pattern p1 = Pattern.compile("abc+d|ab+cd");
+        Pattern p2 = Pattern.compile("a(b+|c+)d");
+
+        // generate random input string
+        List<String> randomInputs = new ArrayList<String>();
+        for (int i = 0; i < 1000; i++) {
+            int size = (int)Math.round(Math.random() * 200);
+            int range = 'Z' - '0' + 1;
+            char gen[] = new char[size];
+            for (int j = 0; j < size; j++) {
+                char x = (char)Math.round(Math.random() * range);
+                gen[j] = x;
+            }
+            randomInputs.add(new String(gen));
+        }
+
+        // test separately
+        int totalRound = 100000;
+        System.out.println("===== test Pattern 1 =====");
+        Date start = new Date();
+        for (int r = 0; r < totalRound; r++) {
+            for (int t = 0; t < randomInputs.size(); t++) {
+                p1.matcher(randomInputs.get(t)).matches();
+            }
+        }
+        System.out.println("Done in " + (new Date().getTime() - start.getTime()) + "ms");
+        System.out.println("===== test Pattern 2 =====");
+        start = new Date();
+        for (int r = 0; r < totalRound; r++) {
+            for (int t = 0; t < randomInputs.size(); t++) {
+                p2.matcher(randomInputs.get(t)).matches();
+            }
+        }
+        System.out.println("Done in " + (new Date().getTime() - start.getTime()) + "ms");
+    }
+}
